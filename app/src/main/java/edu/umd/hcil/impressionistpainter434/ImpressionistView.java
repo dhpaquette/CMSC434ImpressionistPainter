2f9e55f67bad350aaab75f0f637637e72206e4e6
Dylan H. Paquette <dhpaquette@gmail.com>
2016-04-08 18:43:29 -0400

    Third

--- app/src/main/java/edu/umd/hcil/impressionistpainter434/ImpressionistView.java
+++ app/src/main/java/edu/umd/hcil/impressionistpainter434/ImpressionistView.java
@@ -197,20 +197,7 @@ public class ImpressionistView extends View {
         }
 
         return result;
-        /*if (b1.getWidth() == b2.getWidth() && b1.getHeight() == b2.getHeight()) {
-            int[] pixels1 = new int[b1.getWidth() * b1.getHeight()];
-            int[] pixels2 = new int[b2.getWidth() * b2.getHeight()];
-            b1.getPixels(pixels1, 0, b1.getWidth(), 0, 0, b1.getWidth(), b1.getHeight());
-            b2.getPixels(pixels2, 0, b2.getWidth(), 0, 0, b2.getWidth(), b2.getHeight());
-            if (Arrays.equals(pixels1, pixels2)) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }*/
+        
     }
 
 
